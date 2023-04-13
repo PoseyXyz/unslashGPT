c5bacf55ee8266340814a00474140e64bbe53d3f
Adeposi <adeposi60@gmail.com>
2023-04-13 15:53:30 +0400

    Setup next config to ignore build errors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript:{
+    ignoreBuildErrors:true
+  }, 
+  images:{
+    domains:['oaidalleapiprodscus.blob.core.windows.net']
+  }
 }
 
 module.exports = nextConfig
